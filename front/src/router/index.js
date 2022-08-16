397cd09604a673434203f4a2a9042dcb78549761
oeunchansok <oeunchansok@gmail.com>
2022-08-16 19:21:18 +0700

    #8 [Request Form] Create Leave Request Form

---
+++ front/src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import DashBoard from "../components/DashBoard.vue"
+import NewRequest from "../components/NewRequest.vue"
+import CheckListStudent from "../components/CheckList.vue"
+import StudentList from "../components/StudentList.vue"
+
+const routes = [
+  {
+    path: '/',
+    name: 'dashboard',
+    component: DashBoard
+  },
+  {
+    path: '/request',
+    name: 'request',
+    component: NewRequest
+  },
+  {
+    path: '/checkList',
+    name: 'checkList',
+    component: CheckListStudent
+  },
+  {
+    path: '/studentlist',
+    name: 'studentlist',
+    component: StudentList
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
